86ae74fe3bae08a05af02d52f3ab895a00efc377
s.saltykov <s.saltykov@dev.userstory.ru>
2019-04-10 08:11:22 +0700

    state

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import UserContainer from 'modules/user/containers/UserContainer/UserContainer';
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <UserContainer />
+      </div>
+    );
+  }
+}
+
+export default App;
